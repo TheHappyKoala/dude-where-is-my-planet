761c589fd5b8d55915bfa36bbaaa2be4695b06f2
Darrell <rather.glorious@gmail.com>
2019-05-15 21:05:41 +0200

    Add method for constructing a query url

---
+++ src/construct-query/constructQuery.spec.ts
@@ -0,0 +1,18 @@
+import constructQuery from './constructQuery';
+import url from '../api';
+
+test('constructs a url with query parameters that return state vectors for Ceres', () => {
+  expect(
+    constructQuery({
+      url,
+      body: 'Ceres',
+      units: 'AU-D',
+      center: '@0',
+      start: '2000-01-01',
+      stop: '2000-12-31',
+      step: '1 d'
+    })
+  ).toBe(
+    `https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1&COMMAND='Ceres'&OUT_UNITS='AU-D'&CENTER=@0&MAKE_EPHEM='YES'&TABLE_TYPE='VECTORS'&START_TIME='2000-01-01'&STOP_TIME='2000-12-31'&STEP_SIZE='1 d'&QUANTITIES='1,9,20,23,24'`
+  );
+});
---
+++ src/construct-query/constructQuery.ts
@@ -0,0 +1,32 @@
+interface Query {
+  url: string;
+  body: string;
+  units: string;
+  center: string;
+  start: string;
+  stop: string;
+  step: string;
+}
+
+export default ({
+  url,
+  body,
+  units,
+  center,
+  start,
+  stop,
+  step
+}: Query): string => {
+  return [
+    `${url}`,
+    `&COMMAND='${body}'`,
+    `&OUT_UNITS='${units}'`,
+    `&CENTER=${center}`,
+    `&MAKE_EPHEM='YES'`,
+    `&TABLE_TYPE='VECTORS'`,
+    `&START_TIME='${start}'`,
+    `&STOP_TIME='${stop}'`,
+    `&STEP_SIZE='${step}'`,
+    `&QUANTITIES='1,9,20,23,24'`
+  ].join('');
+};
